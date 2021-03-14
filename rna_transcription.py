b6b6664dd7478446b2867e191ec7c5b2d3a89f1f
John Chen <john.person1@gmail.com>
2021-03-13 22:56:40 -0500

    implement RNA seq to antisense, template strand

--- rna_transcription.py
+++ rna_transcription.py
@@ -13,6 +13,20 @@ def generate_template_strand(rna_input_seq):
     :param rna_input_seq:
     :return:
     '''
-    return
+    RNA_TO_DNA_MAPPING = {
+
+        "A": "T",
+        "C": "G",
+        "G": "C",
+        "U": "A"
+    }
+    DNA_seq = []
+    for char in rna_input_seq:
+        DNA_seq.append(RNA_TO_DNA_MAPPING[char])
+    RNA_seq = reversed(DNA_seq)
+    return "".join(RNA_seq)
 
 
+
+if __name__ == "__main__":
+    print(generate_template_strand("AUGCCCAACAGCAAGAGUGGUGCCCUGUCGAAGGAG"))
