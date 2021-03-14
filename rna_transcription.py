496ba7353a9b5e81db7031d3181064fa7141a7be
John Chen <john.person1@gmail.com>
2021-03-13 22:47:21 -0500

    add initial structure for function

---
+++ rna_transcription.py
@@ -0,0 +1,18 @@
+
+def generate_coding_strand(rna_input_seq):
+    '''
+    Generates the DNA coding strand for a given RNA input seq
+    :param rna_input_seq:
+    :return:
+    '''
+    return
+
+def generate_template_strand(rna_input_seq):
+    '''
+    Generates the DNA template strand for a given RNA input seq
+    :param rna_input_seq:
+    :return:
+    '''
+    return
+
+
